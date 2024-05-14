232b94dd9477100b9ef82b251af2d4b36964148a
Akhil Valsamrithan <avalsamrithan@rbbn.com>
2024-05-14 12:05:26 +0530

    Changed logging overwrite

--- fetch_and_push.sh
+++ fetch_and_push.sh
@@ -101,7 +101,7 @@ function fetch_repo() {
         echo "Pushing changes to $REMOTE"
         sshpass -p "$SEC_PASS" git push "$REMOTE" -f --all
         validate_execution "git push failed!"
-    } >> "$LOG_FILE" 2>&1 &
+    } > "$LOG_FILE" 2>&1 &
 }
 
 # Main
